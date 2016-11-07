38ee39cf304adeaaea9c7a34713bf4638a9c14b8
QuorumAppInsights <alex_compton@TAMLACOMPTON2>
2016-11-07 10:48:06 -0500

    using real async

--- jscallback/script4.js
+++ jscallback/script4.js
@@ -2,24 +2,17 @@
 var str4 = "";
 
 var funcs = [];
-var funcs2 = [];
-
-function createfunc(i, delay) {
-    return function() { 
-        str4 += i + ': ' + delay + 'ms</br>'; 
-    };
-}
 
 function printString(i,delay){
     str4 += i + ': ' + delay + 'ms</br>'; 
     document.getElementById("script4").innerHTML = str4;
 }
 
-function createfunc2(i, delay, callback) {
+function createfunc(i, delay, callback) {
     return function() { 
         setTimeout(function () {            
             callback && callback(i,delay);
-        }, 3000);        
+        }, delay);        
     };
 }
 
@@ -28,7 +21,7 @@ str4 += '<h4>iterations</h4>';
 for (var i = 0; i < 10; i++) {
     var delay = Math.round((Math.random() * 3000));
     str4 += i + ': ' + delay + 'ms</br>';
-    funcs[i] = createfunc2(i, delay, printString);
+    funcs[i] = createfunc(i, delay, printString);
 }
 
 document.getElementById("script4").innerHTML = str4;
